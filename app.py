c952c06d5f502e267e53a393e6ef5d636a6cbe94
Castic Jehin <jehincastic@gmail.com>
2021-01-10 18:22:30 +0530

    Alignment Fixed

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ class MyController(Controller):
 	def __init__(self, ** kwargs):
 		Controller.__init__(self, ** kwargs)
 
-def on_x_press(self):
-	led.value = brightnessValue
+	def on_x_press(self):
+		led.value = brightnessValue
 
-def on_circle_press(self):
-	brightnessValue = 0
-	led.value = brightnessValue
+	def on_circle_press(self):
+		brightnessValue = 0
+		led.value = brightnessValue
 
-def on_L3_up(self, value):
-	brightnessValue = abs(value) / maxValue
-	led.value = brightnessValue
+	def on_L3_up(self, value):
+		brightnessValue = abs(value) / maxValue
+		led.value = brightnessValue
 
 controller = MyController(interface = "/dev/input/js0", connecting_using_ds4drv = False)
 controller.listen()
