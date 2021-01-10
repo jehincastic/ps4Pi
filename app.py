14d3bb4af6d465f1bd216d08b97e126b89ced203
Castic Jehin <jehincastic@gmail.com>
2021-01-10 18:14:17 +0530

    Add Brightness

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
-from pyPS4Controller.controller import Controller
-from gpiozero import LED
-from time import sleep
+from pyPS4Controller.controller
+import Controller
+from gpiozero
+import LED
+from time
+import sleep
 
 led = LED(4)
+brightnessValue = 1
+maxValue = 32767
 
 class MyController(Controller):
-    def __init__(self, **kwargs):
-        Controller.__init__(self, **kwargs)
+	def __init__(self, ** kwargs):
+	Controller.__init__(self, ** kwargs)
 
-    def on_x_press(self):
-        led.on()
+def on_x_press(self):
+	led.value = brightnessValue
 
-    def on_circle_press(self):
-        led.off()
+def on_circle_press(self):
+	brightnessValue = 0
+	led.value = brightnessValue
 
-controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
+def on_L3_up(self, value):
+	brightnessValue = abs(value) / maxValue
+	led.value = brightnessValue
+
+controller = MyController(interface = "/dev/input/js0", connecting_using_ds4drv = False)
 controller.listen()
-
